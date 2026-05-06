f7ac12d23105f7a6b35935368b21d78b996afda3
Devansh Mishra <dkmishra2701@gmail.com>
2026-05-06 23:50:23 +0530

    Commit

--- a2 cobra/UpperCaseClient.java
+++ a2 cobra/UpperCaseClient.java
@@ -9,25 +9,36 @@ class UpperCaseClient {
     public static void main(String args[]) {
         try {
             // Initialize ORB
-            ORB orb = ORB.init(args, null);
+//            ORB orb = ORB.init(args, null);
+//
+//            // Get naming context
+//            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+//            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+//
+//            // Resolve object
+//            String name = "UpperCase";
+//            UpperCase upperImpl = UpperCaseHelper.narrow(ncRef.resolve_str(name));
+//
+//            // Input from user
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//            System.out.print("Enter String: ");
+//            String str = br.readLine();
+//
+//            // Call remote method
+//            String result = upperImpl.ConvertToUppercase(str);
+//
+//            System.out.println("Uppercase String: " + result);
 
-            // Get naming context
-            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            ORB orb = ORB.init(args,null);
 
-            // Resolve object
+            org.omg.CORBA.Object objref = orb.resolve_initial_reference("NameService");
+            NamingContextExt ncref = NamingContextExtHelper.narrow(objref);
+
             String name = "UpperCase";
-            UpperCase upperImpl = UpperCaseHelper.narrow(ncRef.resolve_str(name));
 
-            // Input from user
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Enter String: ");
-            String str = br.readLine();
+            UpperCase upperimpl = UpperCaseHelper.narrow(ncref.resolve_str(name));
 
-            // Call remote method
-            String result = upperImpl.ConvertToUppercase(str);
 
-            System.out.println("Uppercase String: " + result);
 
         } catch (Exception e) {
             System.out.println("ERROR: " + e);
--- a2 cobra/UpperCaseServer.java
+++ a2 cobra/UpperCaseServer.java
@@ -1,42 +1,76 @@
+//import UpperCaseModule.*;
+//import org.omg.CosNaming.*;
+//import org.omg.CORBA.*;
+//import org.omg.PortableServer.*;
+//
+//class UpperCaseServer {
+//    public static void main(String args[]) {
+//        try {
+//            // Initialize ORB
+//            ORB orb = ORB.init(args, null);
+//
+//            // Get reference to RootPOA
+//            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+//            rootpoa.the_POAManager().activate();
+//
+//            // Create servant
+//            UpperCaseImpl upperImpl = new UpperCaseImpl();
+//
+//            // Get object reference
+//            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(upperImpl);
+//            UpperCase href = UpperCaseHelper.narrow(ref);
+//
+//            // Get naming context
+//            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+//            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+//
+//            // Bind object
+//            String name = "UpperCase";
+//            NameComponent path[] = ncRef.to_name(name);
+//            ncRef.rebind(path, href);
+//
+//            System.out.println("UpperCase Server ready and waiting...");
+//
+//            // Wait for client calls
+//            orb.run();
+//
+//        } catch (Exception e) {
+//            System.out.println("ERROR: " + e);
+//            e.printStackTrace();
+//        }
+//    }
+//}
+
+
 import UpperCaseModule.*;
-import org.omg.CosNaming.*;
 import org.omg.CORBA.*;
+import org.omg.CosNaming.*;
 import org.omg.PortableServer.*;
 
-class UpperCaseServer {
-    public static void main(String args[]) {
-        try {
-            // Initialize ORB
-            ORB orb = ORB.init(args, null);
+public class UpperCaseServer {
+    static void main(String[] args) {
+        try{
+            ORB orb = ORB.init(args,null);
 
-            // Get reference to RootPOA
-            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            POA rootpoa =  POAHelper.narrow(orb.resolve_initial_reference("RootPOA"));
             rootpoa.the_POAManager().activate();
 
-            // Create servant
-            UpperCaseImpl upperImpl = new UpperCaseImpl();
+            UpperCaseImpl upperimpl = new UpperCaseImpl();
 
-            // Get object reference
-            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(upperImpl);
-            UpperCase href = UpperCaseHelper.narrow(ref);
+            org.omg.CORBA.Object obj = rootpoa.servant_to_reference(upperimpl);
+            UpperCase href = UpperCaseHelper.narrow(obj);
 
-            // Get naming context
-            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            org.omg.CORBA.Object objref = orb.resolve_initial_reference("NameService");
+            NamingContextExt ncref = NamingContextExtHelper.narrow(objref);
 
-            // Bind object
             String name = "UpperCase";
-            NameComponent path[] = ncRef.to_name(name);
-            ncRef.rebind(path, href);
+            NamingContext[] path = href.to_name(name);
+            ncref.rebind(path);
 
-            System.out.println("UpperCase Server ready and waiting...");
-
-            // Wait for client calls
+            System.out.println("Server running");
             orb.run();
-
         } catch (Exception e) {
-            System.out.println("ERROR: " + e);
-            e.printStackTrace();
+            System.out.println(e);
         }
     }
 }
